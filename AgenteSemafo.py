5e97e3fce9b6968a4aaa63e900011e3cf39cba95
ronal mauricio aguirre machic <ronalaguirre97@gmail.com>
2025-02-21 17:36:18 -0600

    UPDATE , Agente semafóro , se agrego condición de cambio

--- AgenteSemafo.py
+++ AgenteSemafo.py
@@ -14,3 +14,14 @@ def agente_semaforo():
 
         print(f"Estado: {estado_actual}, Vehículos detectados: {vehiculos}, Tiempo: {tiempo}s")
         time.sleep(tiempo)
+
+        if estado_actual == 'Verde':
+            estado_actual = 'Amarillo'
+        elif estado_actual == 'Amarillo':
+            estado_actual = 'Rojo'
+        else:
+            estado_actual = 'Verde'
+
+# Ejecutar el agente de semáforo inteligente
+if __name__ == "__main__":
+    agente_semaforo()
